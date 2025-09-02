0112a52df9030e48c73897bda804f0242e2be364
Atharvaa99 <atharvadhaigude@icloud.com>
2025-09-02 19:53:12 +0530

    Stage 3 & 4

---
+++ stage-3.php
@@ -0,0 +1,19 @@
+<?php
+// Read input
+/*
+$appName = trim(fgets(STDIN));
+echo "Welcome to the $appName!";
+*/
+
+$billAmount = (float)(fgets(STDIN));
+$tipPercentage = (int)(fgets(STDIN));
+// $numberOfPeople = (int)(fgets(STDIN));
+
+echo "Bill Amount: $$billAmount\n";
+echo "Tip Percentage: $tipPercentage%\n";
+//echo "Number of People: $numberOfPeople\n";
+
+$tipAmount = ($billAmount * $tipPercentage) / 100;
+echo "Tip Amount: $$tipAmount";
+
+?>
---
+++ stage-4.php
@@ -0,0 +1,22 @@
+<?php
+// Read input
+/*
+$appName = trim(fgets(STDIN));
+echo "Welcome to the $appName!";
+*/
+
+$billAmount = (float)(fgets(STDIN));
+$tipPercentage = (int)(fgets(STDIN));
+// $numberOfPeople = (int)(fgets(STDIN));
+
+echo "Original Bill: $$billAmount\n";
+//echo "Tip Percentage: $tipPercentage%\n";
+//echo "Number of People: $numberOfPeople\n";
+
+$tipAmount = ($billAmount * $tipPercentage) / 100;
+echo "Tip Amount: $$tipAmount\n";
+
+$totalBill = $billAmount + $tipAmount;
+echo "Total Bill: $$totalBill";
+
+?>
